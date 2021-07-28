4f8baa3026feeca7e70b9f312ccf26dec280beee
Alex On <shuhia.on@gmail.com>
2021-07-29 00:00:58 +0200

    add scripts for github deployment

---
+++ src/components/cards/TestimonialCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const styles = { container: { display: "flex", flexFlow: "row wrap" } };
+
+const TestimonialCard = () => {
+  return (
+    <div styles={styles.container}>
+      <img />
+      <div className="rating"></div>
+      <div className="comment"></div>
+      <div className="name"></div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
