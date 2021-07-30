0baf4eb2fc3c4a5585295a1ce879ac86fc84cb09
Alex On <shuhia.on@gmail.com>
2021-07-30 11:41:03 +0200

    add: responsive navbar

--- src/components/navigation/NavItem.js
+++ src/components/navigation/NavItem.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavItem(props) {
-  const { name = "name", href = "/" } = props;
+  const { name = "", href = "/" } = props;
   return (
     <li className="main-nav-item">
-      <Link to={href}>{name}</Link>
+      <Link to={href}>
+        {name}
+        {props.children}
+      </Link>
     </li>
   );
 }
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -2,23 +2,58 @@
 import React from "react";
 import NavItem from "./NavItem";
 import logo from "../../images/logo.svg";
+import menu from "../../images/menu.svg";
+import clearMenu from "../../images/clear-menu.svg";
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const onMobile = useMediaQuery({ query: "(max-width:800px)" });
+  const navItems = (
+    <>
+      <NavItem name="Testing ground" href="/test"></NavItem>
+      <NavItem name="Discover"></NavItem>
+      <NavItem name="About"></NavItem>
+      <NavItem name="Contact us"></NavItem>
+      <NavItem>
+        <img className="profile" src="./profile.svg"></img>
+      </NavItem>
+      <NavItem>
+        <img className="cart" src="./cart.svg"></img>
+      </NavItem>
+    </>
+  );
+  // Reset toggle
+  useEffect(() => {
+    if (!onMobile) {
+      setShowMenu(false);
+    }
+  }, [onMobile]);
+
   return (
     <nav className="main-nav">
-      <span>
-        <img className="logo" src={logo}></img>
-      </span>
-      <ul className="main-nav-items">
-        <NavItem name="Testing ground" href="/test"></NavItem>
-        <NavItem name="Discover"></NavItem>
-        <NavItem name="About"></NavItem>
-        <NavItem name="Contact us"></NavItem>
+      {onMobile && (
+        <span style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+          <span
+            onClick={() => {
+              setShowMenu((prev) => !prev);
+            }}
+          >
+            {!showMenu ? <img src={menu}></img> : <img src={clearMenu}></img>}
+          </span>
+          <img style={{ margin: "auto" }} className="logo" src={logo}></img>
+        </span>
+      )}
+      <ul className="main-nav-ul">
+        {onMobile && showMenu && <>{navItems}</>}
+        {!onMobile && (
+          <>
+            <img className="logo" src={logo}></img>
+            {navItems}
+          </>
+        )}
       </ul>
-      <span className="right-menu">
-        <img className="profile" src="./profile.svg"></img>
-        <img className="cart" src="./cart.svg"></img>
-      </span>
     </nav>
   );
 }
