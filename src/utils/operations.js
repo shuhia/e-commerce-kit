74bbffefa6dcca497f4c6022892d5e8ae6c3151a
Alex On <shuhia.on@gmail.com>
2021-08-04 21:52:40 +0200

    move replace function to util

--- src/utils/operations.js
+++
@@ -1,4 +0,0 @@
-function replaceItemIn(array, newItem) {
-  return array.map((prev) => (prev.id === newItem.id ? newItem : prev));
-}
-
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,11 @@
 import { randomProduct, randomProducts } from "./product";
 
+function replaceItemIn(array, newItem) {
+  return array.map((prev) => (prev.id === newItem.id ? newItem : prev));
+}
+
 function randomNumber(min = 0, max = 10) {
   return min + Math.floor(Math.random() * max + 1);
 }
 
-export { randomNumber, randomProduct, randomProducts };
+export { randomNumber, randomProduct, randomProducts, replaceItemIn };
