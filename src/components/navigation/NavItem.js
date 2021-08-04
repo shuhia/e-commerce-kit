163d22c35b9401385120df39b4af1cf5a520faad
Alex On <shuhia.on@gmail.com>
2021-08-04 23:02:40 +0200

    add prop active

--- src/components/navigation/NavItem.js
+++ src/components/navigation/NavItem.js
@@ -3,13 +3,20 @@ import { Link } from "react-router-dom";
 import styles from "./navItem.module.css";
 
 function NavItem(props) {
-  const { name = "", href = "/" } = props;
+  const { name = "", href = "/", active = true } = props;
   return (
     <li className={styles.container}>
-      <Link to={href}>
-        {name}
-        {props.children}
-      </Link>
+      {active ? (
+        <Link to={href}>
+          {name}
+          {props.children}
+        </Link>
+      ) : (
+        <span style={{ backgroundColor: "grey" }}>
+          {name}
+          {props.children}
+        </span>
+      )}
     </li>
   );
 }
