1f2dd1bcd4f430805cfc28292f90ffc6a8f769e3
Alex On <shuhia.on@gmail.com>
2021-08-04 20:55:56 +0200

    Add functions and documentations

--- src/contexts/store/StoreProvider.js
+++ src/contexts/store/StoreProvider.js
@@ -8,36 +8,52 @@ function random(min = 0, max = 10) {
 }
 
 function StoreProvider(props) {
-  const product = {
-    name: "Spiced Mint Candleaf ®",
-    id: uuidv4(),
-    img: productImg,
-    price: { value: random(), currency: "$" },
-    quantity: 1,
-  };
+  /**
+   *
+   * @param {String} name Products name.
+   * @param {Number} id Products ID.
+   * @param {Object} img Products Img.
+   * @param {Number} price The price of the product.
+   * @param {Number} quantity The amount of the product.
+   * @returns {Object} Object with follwing keys: name, id, img, price and quantity.
+   */
 
-  const product2 = {
-    name: "Spiced Mint Candleaf ®",
-    id: uuidv4(),
-    img: productImg,
-    price: { value: random(), currency: "$" },
-    quantity: 1,
-  };
-  const products = [product, product2];
-
-  function randomProduct() {
+  function randomProduct(
+    name = "Spiced Mint Candleaf ®",
+    id = uuidv4(),
+    img = { productImg },
+    price = { value: random(), currency: "$" },
+    quantity = 1
+  ) {
     return {
-      name: "Spiced Mint Candleaf ®",
-      id: uuidv4(),
-      img: productImg,
-      price: { value: random(), currency: "$" },
-      quantity: 1,
+      name,
+      id,
+      img,
+      price,
+      quantity,
     };
+  }
+
+  // Returns an array with random products
+  /**
+   *
+   * @param {Number} amount of products.
+   * @returns {Array} amount products in array.
+   */
+  function randomProducts(amount) {
+    return Array(amount)
+      .fill("product")
+      .map(() => randomProduct());
   }
 
   function fetchProducts() {}
 
-  const value = { product, products, randomProduct };
+  // Create 4 random products and store it in products
+  const [products, setProducts] = useState(randomProducts(4));
+
+  const product = products[0];
+
+  const value = { product, products, randomProduct, randomProducts };
   return (
     <StoreContext.Provider value={value}>
       {props.children}
