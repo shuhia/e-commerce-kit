7aedf1fb78c835ae024ca79a78fb2b1ce48a1a1a
Alex On <shuhia.on@gmail.com>
2021-08-04 06:41:33 +0200

    add basic form to shipping

--- src/pages/Shipping.js
+++ src/pages/Shipping.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { useRouteMatch } from "react-router-dom";
+import Order from "../components/sections/Order";
 import BigButton from "../components/buttons/BigButton";
+import { Link } from "react-router-dom";
 
 function Shipping() {
   return (
-    <div>
-      <BigButton
-        name="Go to payment"
-        href={"/checkout" + "/payment"}
-      ></BigButton>
-    </div>
+    <section>
+      <form>
+        <div>
+          <label for="contact">contact</label>
+          <input id="contact"></input>
+          <Link to="details">Edit</Link>
+        </div>
+        <div>
+          <label for="adress">adress</label>
+          <input id="adress"></input>
+          <Link to="details">Edit</Link>
+        </div>
+        <fieldset>
+          <legend>Shipping method</legend>
+          <input id="standardShipping" type="radio" checked></input>
+          <label for="standardShipping">standard Shipping</label>
+        </fieldset>
+        <BigButton
+          name="Go to payment"
+          href={"/checkout" + "/payment"}
+        ></BigButton>
+      </form>
+      <Order></Order>
+    </section>
   );
 }
 
