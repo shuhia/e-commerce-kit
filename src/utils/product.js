5e128da249d3213dca2e243d3019cb392300da9d
Alex On <shuhia.on@gmail.com>
2021-08-04 22:30:08 +0200

    add getPrice

--- src/utils/product.js
+++ src/utils/product.js
@@ -3,21 +3,33 @@ import { v4 as uuidv4 } from "uuid";
 import productImg from "../images/product-1.png";
 
 /**
+ * Summary: Creates a random product.
+ *
+ * Description: This function uses uuid to generate an id.
  *
  * @param {String} name Products name.
  * @param {Number} id Products ID.
  * @param {Object} img Products Img.
- * @param {Number} price The price of the product.
+ * @param {Object} price The price of the product: {value:Number, currency:String}.
+ * @param {Function} getPrice Returns the price of the product in string.
  * @param {Number} quantity The amount of the product.
- * @returns {Object} Object with follwing keys: name, id, img, price and quantity.
+ * @param {Object} options Any properties that are not implemented.
+ * @returns {Object} Object with this format: {name, id, img, price, quantity, options}.
  */
 
 function randomProduct(
   name = "Spiced Mint Candleaf ®",
   id = uuidv4(),
   img = { productImg },
-  price = { value: randomNumber(), currency: "$" },
-  quantity = 1
+  price = {
+    value: randomNumber(),
+    currency: "$",
+  },
+  getPrice = function () {
+    return this.price.value + this.price.currency;
+  },
+  quantity = 1,
+  options = []
 ) {
   return {
     name,
