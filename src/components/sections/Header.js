040fde6668f3b7945fb2b4a18927b309593c0258
Alex On <shuhia.on@gmail.com>
2021-08-06 06:03:01 +0200

    add media queries to header

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,10 +1,30 @@
 import React from "react";
 import BackgroundCard from "../cards/BackgroundCard";
+import styles from "./header.module.css";
+import bgImageMobile from "../../images/bg-image-mobile.png";
+import bgImage from "../../images/bg-image.jpg";
+import LinkButton from "../buttons/LinkButton";
+import { useMediaQuery } from "react-responsive";
 
 function Header() {
+  const isMobile = useMediaQuery({ maxWidth: 800 });
   return (
-    <header className="header">
-      <BackgroundCard></BackgroundCard>
+    <header className={styles.container}>
+      <img
+        className={styles.image}
+        src={isMobile ? bgImageMobile : bgImage}
+        alt="background"
+      ></img>
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          🌱<br></br> The nature candle
+        </h1>
+        <p className={styles.description}>
+          All handmade with natural soy wax, Candleaf is a companion for all
+          your pleasure moments.
+        </p>
+        <LinkButton size="small" name="Discover our products"></LinkButton>
+      </div>
     </header>
   );
 }
